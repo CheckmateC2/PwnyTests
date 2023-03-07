a76ba6251ed34d2a9d7682cd2fd7b99b0c8a419b
Ivan Nikolsky <enty8080@gmail.com>
2023-03-07 14:24:21 +0300

    Update console.py

--- console.py
+++ console.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+##
+#
+# This test works only on Linux distro, because of
+#   session.details['Platform'] = "linux"
+#
+##
+
 import socket
 
 from pwny.session import PwnySession
@@ -10,6 +17,7 @@ sock.listen(1)
 c, _ = sock.accept()
 
 session = PwnySession()
+session.details['Platform'] = "linux"
 session.open(c)
 
 session.interact()
